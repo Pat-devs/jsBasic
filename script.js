c3eb8eefb1b0c707d7d58dafc99c3193f2416b5d
Pat-devs <patryk.kodehode@gmail.com>
2026-01-08 13:14:15 +0100

    querySelctor

--- script.js
+++ script.js
@@ -2,5 +2,15 @@
 
 // select html elements with JS
 
-// 1. how to select by a tag name ?
+// 1. how to select by a tag name ? (like CSS, select h1 { ... }) in JS...
 
+
+let h1Element = document.querySelector("h1")
+
+console.log(h1Element)
+
+// select p tag (NOTE: unlike in CSS, this will only select the first p tag). To select multiple requires a different method, more on that later :)
+
+let paragraphElement = document.querySelector("p")
+
+console.log(paragraphElement)
