be6f5d9c32adc8b41ef93f5425ae57481068da75
Pat-devs <patryk.kodehode@gmail.com>
2026-01-08 14:02:47 +0100

    simple calc

--- script.js
+++ script.js
@@ -1,72 +1,14 @@
-// DOM (Document object model) introduction
-
-// select html elements with JS
-
-// 1. how to select by a tag name ? (like CSS, select h1 { ... }) in JS...
 
 
-let h1Element = document.querySelector("h1")
+let number1Element = document.querySelector("#number-1")
 
-console.log(h1Element)
-
-// select p tag (NOTE: unlike in CSS, this will only select the first p tag). To select multiple requires a different method, more on that later :)
-
-let paragraphElement = document.querySelector("p")
-
-console.log(paragraphElement)
-
-// 2. How to select elements by class name ?
-
-let numbersClassElement = document.querySelector(".numbers")
-
-console.log(numbersClassElement)
-
-// 3. How to select element by id ?
-
-let numbersIdElement = document.querySelector("#numbers")
-
-console.log(numbersIdElement)
+let number2Element = document.querySelector("#number-2")
 
 
-// how to change an element ?
-h1Element.textContent = 'Javascript' // to change or remove text
+let sumElement = document.querySelector("#sum")
 
-paragraphElement.innerHTML = `<b>Hei</b>` // innerHTML allows us to create html tags with javascript
-
-
-let users = [
-  "Tom",
-  "Eric",
-  "Jessica",
-  "Scott",
-  "Anna",
-  "Carl",
-  "Elisabeth",
-  "Benny",
-  "Oliver",
-  "Andy",
-  "Jenny",
-  "Ashley",
-  "Erin",
-  "Patrick",
-];
-
-let noeText = "Hei"
-noeText += " og hadet!"
-
-console.log(noeText)
+number1Element.textContent = 5
+number2Element.textContent = "5"
 
 
-document.body.innerHTML += `<p>${noeText}</p>`
-
-
-
-// select the div container
-
-containerElement = document.querySelector(".container")
-
-
-for (const user of users) {
-    
-    containerElement.innerHTML += `<li>${user}</li>`
-}
+sumElement.textContent = Number(number1Element.textContent) + Number(number2Element.textContent)
